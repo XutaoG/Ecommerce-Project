7bb0a67e3b40c9dec3d2471b1624dbf0a1e5ec7f
Xutao <taogeeee@gmail.com>
2024-01-29 11:12:01 -0500

    Updated package config

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default 
+{
+	// OpenID connect
+	oidc:
+	{
+		clientId: "0oaer81tyo4Odzg8W5d7",
+		issuer: "https://dev-96600962.okta.com/oauth2/default",
+		redirectUri: "http://localhost:4200/login/callback",
+		scopes: ["openid", "profile", "email"]
+	}
+}
